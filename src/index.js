014d907f7c0565016dd619a36c05b3620f3c0937
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-27 15:04:40 -0300

    control script finished. Analogclock is complete.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+setInterval(setClock, 1000)
+
+const hoursHand = document.querySelector('[data-hours-hand]')
+const minutesHand = document.querySelector('[data-minutes-hand]')
+const secondsHand = document.querySelector('[data-seconds-hand]')
+
+function setClock() {
+	const currentDate = new Date()
+	const secondsRatio = currentDate.getSeconds() / 60
+	const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+	const hoursRatio = (minutesRatio + currentDate.getHours()) / 60
+	setRotation(secondsHand, secondsRatio)
+	setRotation(minutesHand, minutesRatio)
+	setRotation(hoursHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+	element.style.setProperty('--rotation', rotationRatio * 360)
+}
